36b10bfcfecc8e32187326f3a1a682a8c0fee93d
Jacob McFaul <mcjakedude@gmail.com>
2019-07-22 15:46:31 -0700

    first commit

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -18,7 +18,7 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010, # LDI R0,8 
             0b00000000,
             0b00001000,
             0b01000111, # PRN R0
